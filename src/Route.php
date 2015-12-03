367862017f2a7fa0444330484176f797fa40ccec
nickstuer <nickstuer@gmail.com>
2015-12-03 18:29:24 +0000

    added comments

--- src/Route.php
+++ src/Route.php
@@ -4,10 +4,28 @@ namespace NickStuer\EasyRouter;
 
 class Route
 {
+    /**
+     * @var string $httpMethod
+     */
     private $httpMethod;
+
+    /**
+     * @var string $route
+     */
     private $route;
+
+    /**
+     * @var string $action
+     */
     private $action;
 
+    /**
+     * Route constructor.
+     *
+     * @param $httpMethod
+     * @param $route
+     * @param $action
+     */
     public function __construct($httpMethod, $route, $action)
     {
         $this->httpMethod = $httpMethod;
@@ -15,16 +33,25 @@ class Route
         $this->action = $action;
     }
 
+    /**
+     * @return string
+     */
     public function getHttpMethod()
     {
         return $this->httpMethod;
     }
 
+    /**
+     * @return string
+     */
     public function getRoute()
     {
         return $this->route;
     }
 
+    /**
+     * @return string
+     */
     public function getAction()
     {
         return $this->action;
